6af40b82a344223a7c2e44faf0ba33a5b09d6daa
Tyler Nass <tyler.nass30@gmail.com>
2018-11-14 10:18:43 -0600

    Create name.sol

---
+++ Solidity Fundamentals/name.sol
@@ -0,0 +1,15 @@
+# Program to return name
+
+pragma solidity ^0.4.0;
+
+contract MyFirstContract {
+    string private name;
+    uint private age;
+    
+    function setName(string newName) public {
+        name = newName; // Sets value of name to whatever we set name as
+    }
+    function getName() returns (string) {
+        return name;
+    }
+}
