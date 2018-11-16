4ce339890807b6589947ce9458019490c3190bed
Tyler Nass <tyler.nass30@gmail.com>
2018-11-15 20:10:25 -0600

    Create dataStorage.sol

---
+++ dataStorage.sol
@@ -0,0 +1,12 @@
+/* 
+Design:
+- DB built on top of blockchain
+- Class for functions
+- Functions include: 
+    - store data in the block
+    - receive data in the database
+    - delete data from the database
+    
+Tasks:
+- Create a class
+*/
